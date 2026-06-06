b473e8b53d25c285ec26526e29a06842cdc7dd7a
Alok T <alok.tiwari@infocratsweb.com>
2026-06-06 23:28:00 +0530

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo This is the called method',
       theme: ThemeData(
         // This is the theme of your application.
         //
