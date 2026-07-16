3c1d766c23c1c01317ec5ee47d6a132aadd33e94
Alok T <alok.tiwari@infocratsweb.com>
2026-07-16 15:21:15 +0530

    added const

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
       context,
       MaterialPageRoute<void>(
         builder: (_) =>
-         UserProfileWidget(),
+            const UserProfileWidget(),
       ),
     );
   }
